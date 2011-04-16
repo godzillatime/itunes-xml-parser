f97e7b39b21ae30272c1767f2dde9c22309fa482
Watchboy <watchboy.es@gmail.com>
2011-04-16 19:05:11 -0400

    Added getNewTime function, function descriptions

--- test.py
+++ test.py
@@ -6,13 +6,34 @@ w = open('C:/users/Domas/Music/iTunes/backup/that.xml', 'w')
 info = []
 
 def parseloc (inloc):
+    '''
+    Receive: Location string from iTunes XML file
+    Return:  Location string formatted for the filesystem
+    '''
+    pass
     
+def getNewDate(filePath):
+    '''
+    Receive: Song file location, parsed for special characters
+    Return:  Date created as string formatted for iTunes XML file
+        Format: yyyy-mm-ddThh:mm:ssZ
+    '''
+    t = os.path.getctime(filePath) # Float representing seconds since epoch
+    newDate = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime(t)) 
+
+    return newDate
 
 def parsedate (indate):
+    '''
+    Receive: Date created from iTunes XML file
+    Return:  ???
+    '''
     indate = indate.lstrip()
-    
 
 def dump(info):
+    '''
+    Receive: List of information from the iTunes XML file
+    '''
     count = 0
     for i in info:
         if i.lstrip()[0:13] == '<key>Location':
